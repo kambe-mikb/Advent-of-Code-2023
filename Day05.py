1c914e1f6857784ed9260ffc140866ceca31ff9b
Mike Benson <mike@kambe.com.au>
2023-12-07 07:06:32 +1100

    First Solution ot Day 5, Part 2

--- Day05.py
+++ Day05.py
@@ -149,11 +149,41 @@ So, the lowest location number in this example is 35.
 What is the lowest location number that corresponds to any of the initial seed
 numbers?
 
-Your puzzle answer was 31599214."""
+Your puzzle answer was 31599214.
 
+Part 2
+------
+
+Everyone will starve if you only plant such a small number of seeds. Re-reading
+the almanac, it looks like the seeds: line actually describes ranges of seed
+numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the
+first value is the start of the range and the second value is the length of the
+range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+
+This line describes two ranges of seed numbers to be planted in the garden. The
+first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91,
+92. The second range starts with seed number 55 and contains 13 values: 55, 56,
+..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of
+27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed
+number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77,
+temperature 45, humidity 46, and location 46. So, the lowest location number is
+46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line
+of the almanac. What is the lowest location number that corresponds to any of
+the initial seed numbers?
+"""
+
+from itertools import chain
 from collections import namedtuple
-from functools import cache
-from pprint import pprint
 import typing as T
 
 
@@ -206,7 +236,6 @@ input = [
 MapRange = namedtuple("MapRange", ["dest", "source_lower", "source_length"])
 
 
-# We cache the result because it doesn't change between runs
 def get_almanac(input: T.Iterable) -> dict:
     almanac = {}
     lists = []
@@ -242,7 +271,6 @@ def map_number(mapping: list[MapRange], source: int) -> int:
 
 
 def part_1(input: T.Iterable) -> int:
-    total = 0
     almanac = get_almanac(input)
     locations = []
     for seed in almanac["seeds"]:
@@ -262,9 +290,25 @@ def part_1(input: T.Iterable) -> int:
 
 
 def part_2(input: T.Iterable) -> int:
-    total = 0
     almanac = get_almanac(input)
-    return total
+    locations = []
+    seeds = almanac["seeds"]
+    for seed in chain.from_iterable(
+        [range(s, s + r) for s, r in zip(seeds[::2], seeds[1::2])]
+    ):
+        num = seed
+        for crossref in [
+            "seed-to-soil",
+            "soil-to-fertilizer",
+            "fertilizer-to-water",
+            "water-to-light",
+            "light-to-temperature",
+            "temperature-to-humidity",
+            "humidity-to-location",
+        ]:
+            num = map_number(almanac[crossref], num)
+        locations.append(num)
+    return min(locations)
 
 
 if __name__ == "__main__":
@@ -272,7 +316,7 @@ if __name__ == "__main__":
     print(f"Result of Part 1 (test) = {part_1(input)}")
     print()
     print(f"Result of Part 1 (data) = {part_1(getInput(puzzle_input))}")
-    # print()
-    # print(f"Result of Part 2 (test) = {part_2(input)}")
-    # print()
-    # print(f"Result of Part 2 (data) = {part_2(getInput(puzzle_input))}")
+    print()
+    print(f"Result of Part 2 (test) = {part_2(input)}")
+    print()
+    print(f"Result of Part 2 (data) = {part_2(getInput(puzzle_input))}")
